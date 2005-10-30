ae3d811816c31b05014853a7e31a311d5944555d
Robert Norris <rob@cataclysm.cx>
2005-10-30 23:50:59 +0000

    read
    git-svn-id: svn+ssh://cataclysm.cx/home/fce2/svn/repos/amber@22 ed6cb1fb-4f03-0410-806b-8831b9565825

--- modules/File.c
+++ modules/File.c
@@ -45,9 +45,58 @@ static JSBool file_close(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, 
     return JS_TRUE;
 }
 
+static JSBool file_read(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+    FILE *f;
+    char *buf;
+    int want, len, pos;
+    JSString *str;
+
+    if((f = JS_GetPrivate(cx, obj)) == NULL)
+        return JS_TRUE;
+
+    if(argc == 0)
+        want = -1;
+
+    else if(JS_ValueToInt32(cx, argv[0], &want) == JS_FALSE)
+        return JS_TRUE;
+
+    if(want == 0)
+        return JS_TRUE;
+
+    buf = NULL; len = pos = 0;
+    while(!feof(f) && want > 0 && pos < want) {
+        if(len < pos + 1024) {
+            buf = (char *) JS_realloc(cx, buf, sizeof(char) * (len + 1024));
+            len += 1024;
+        }
+
+        if(want > 0 && want - pos < 1024)
+            pos += fread(&(buf[pos]), sizeof(char), want - pos, f);
+        else
+            pos += fread(&(buf[pos]), sizeof(char), 1024, f);
+    }
+
+    if(pos == 0) {
+        if(buf != NULL)
+            JS_free(cx, buf);
+        return JS_TRUE;
+    }
+
+    if(pos == len)
+        buf = (char *) JS_realloc(cx, buf, sizeof(char) * (len + 1));
+
+    buf[pos] = '\0';
+
+    str = JS_NewString(cx, buf, pos);
+    *rval = STRING_TO_JSVAL(str);
+
+    return JS_TRUE;
+}
+
 static JSFunctionSpec file_methods[] = {
     { "open",   file_open,  2, 0, 0 },
-    { "close",  file_close, 0, 0, 0 }
+    { "close",  file_close, 0, 0, 0 },
+    { "read",   file_read,  1, 0, 0 }
 };
 
 static JSBool file_constructor(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
